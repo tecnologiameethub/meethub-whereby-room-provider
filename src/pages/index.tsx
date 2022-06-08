e7cf9be74e05d112a6a26f9e58585197f9186734
Augusto Dias <me@gutodias.dev>
2022-06-08 10:44:11 -0300

    refactor(style): change grid xl to 3 columns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -130,7 +130,7 @@ const Home: NextPage = () => {
               base: 'repeat(1, 1fr)',
               md: 'repeat(2, 1fr)',
               lg: 'repeat(3, 1fr)',
-              xl: 'repeat(4, 1fr)',
+              xl: 'repeat(3, 1fr)',
             }}
             p={8}
             gap={8}
